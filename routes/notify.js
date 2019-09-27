0515d6a37951d89ea58a27881d7d4a901e0447df
Winson Wang <winwang@paypal.com>
2019-09-27 17:25:41 +0800

    add webhook listener

--- routes/notify.js
+++ routes/notify.js
@@ -2,20 +2,35 @@ var express = require('express');
 var router = express.Router();
 var WXMessage = require('../wx/wxTemplateMessage');
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
 router.post('/ipn', function(req, res, next) {
   // res.render('index', { title: 'Express' });
   var ipnMessage = '';
   var message = new WXMessage();
   //hard code WX user Open ID
   var userId ='o_YTOwqXpFMjQfyJEWMwHwKm1Z6I';
-  var templateId = 'hkCBYtJ6swMwE4i6eRP9MB2uCn9tR2vyOExm-CKsDWI';
+  var templateId = 'pk03pI4VUh9C6YaL1iMndVf6RZQOe9bcvqYY2XGF9-M';
   message.send(userId, templateId).then(function(res1) {
     res.send(res1);
   });
+});
+
+router.post('/webhook', function(req, res, next) {
+  console.log(req.body);
+  var wh = req.body;
+  var info = {};
+  if (wh.event_type === 'PAYMENT.CAPTURE.COMPLETED') {
+    info.id = wh.resource.id;
+    info.amount = wh.resource.amount;
+    info.status = wh.resource.status;
+    var message = new WXMessage();
+    //hard code WX user Open ID
+    var userId ='o_YTOwiNbq-DmM1bIzq06Gae-K3U';
+    var templateId = 'pk03pI4VUh9C6YaL1iMndVf6RZQOe9bcvqYY2XGF9-M';
+    message.send(userId, templateId, info).then(function(res1) {
+      res.send(res1);
+    });
+  }
+  res.send('OK');
 });
 
 module.exports = router;
--- wx/wxTemplateMessage.js
+++ wx/wxTemplateMessage.js
@@ -5,23 +5,24 @@ var templateMessage = function () {
 };
 
 var sendTemplateMessageUrl = "https://api.weixin.qq.com/cgi-bin/message/template/send";
-var accessToken = "25_ZAgK5VlGoYZjHsMhz-IfVamaeng7GlY6-E9pzikgDx88HQobEodcGOOJn6nQb2LVdxKkjd_SlKt-JQBGJUy5Yj7q50dvwWUNpYgUjOdYieJvmw43b5NSkGh7jvAscGnQgUkInu3cdSrwg9EZMWPjAHAVFY";
+var accessToken = "25__SBxmAnflVYZ5SozRYwvFpfkUg38q71rBOJ0D23i44MvghPBYGCK_RO0pL0SvyghlS9zA1tikqi9La_GP2psyE3PP_ubNZXAAlMGOzHowKy-VFuYClH27du4SrkwFjXsHYyiBsCg8T_87L9sODAiAEATIF";
+//var getAccessTokenUrl = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxf5d9250271f0c40a&secret=9a1f424d82d19eeb546408966971236f'
 
-templateMessage.prototype.send = function(userOpenId, templateId) {
+templateMessage.prototype.send = function(userOpenId, templateId, info) {
   var message = '123';
   return rq.post({
     uri: sendTemplateMessageUrl + '?access_token=' + accessToken,
     body: {
       touser: userOpenId,
       template_id: templateId,
-      url: "https://www.sandbox.paypal.com",
+      url: "https://www.sandbox.paypal.com/activity/payment/" + info.id,
       data: {
         txnId: {
-          value:"xxxxx",
+          value:info.id,
           color:"#173177"
         },
         amount: {
-          value:"666.00 RMB",
+          value:info.amount.value + ' ' + info.amount.currency_code,
           color:"#173177"
         }
       }
